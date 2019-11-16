763325026945a8d4b669d1348fbc2591fe646e3c
terrygreen0606 <terrygreen0606@outlook.com>
2019-11-17 02:24:08 +0800

    Changed mongoURI

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI : "mongodb://localhost:27017/mern_starter"
+    // mongoURI : "mongodb://localhost:27017/mern_starter"
+    mongoURI: "mongodb+srv://terry:dnfldml godqhr123@cluster0-ih475.mongodb.net/mern_boilerplate"
 }
